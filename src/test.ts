412e0236ac54e5e2badda3f5e9c9530298db9e6e
Raisess <raisechif@gmail.com>
2020-10-30 15:01:50 -0300

    centered title in the test

--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,17 @@
 import Display from "./Display";
 
-const display: Display = new Display({ x: 100, y: 20 }, ".");
+const window: any = {
+	width: 100,
+	height: 20
+};
+
+const display: Display = new Display({ x: window.width, y: window.height }, ".");
 
 display.setBgColor(40);
 
-display.setPixel({ x: 1, y: 0 }, "abacaxi", 33);
+const title: string = "center title asdfghjkl";
+
+display.setPixel({ x: (window.width / 2) - (Math.round(title.length / 2)), y: 0 }, title, 33);
 display.setPixel({ x: 10, y: 3 }, "O", 34);
 display.setPixel({ x: 30, y: 12 }, "test", 31);
 
