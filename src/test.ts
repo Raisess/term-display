06568efb69822ee93ec5a43d279ccdca77522f0e
Raisess <raisechif@gmail.com>
2020-10-30 13:52:28 -0300

    fix tests

--- src/test.ts
+++ src/test.ts
@@ -1,18 +1,18 @@
 import Display from "./Display";
 
-const display: Display = new Display({ x: 10, y: 10 }, ".");
+const display: Display = new Display({ x: 100, y: 20 }, ".");
 
-//display.setBgColor(45);
+display.setBgColor(45);
 
 display.setPixel({ x: 1, y: 0 }, "abacaxi", [33, 89]);
+display.setPixel({ x: 10, y: 3 }, "O", [34, 89]);
+display.setPixel({ x: 30, y: 12 }, "test", [31, 89]);
 
 display.show();
 
 console.log("\n");
 
-//display.clearPixel({ x: 10, y: 3 });
-
-display.setBgColor(46);
+display.clearPixel({ x: 10, y: 3 });
 
 display.show();
 
